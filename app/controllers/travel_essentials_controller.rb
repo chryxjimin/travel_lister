6d7adfd1cbd0f700ecc21fa5a6c87146b0df5048
Your Username <you@example.com>
2020-11-05 13:49:30 -0500

    created homepage with  get '/' do and index.erb

---
+++ app/controllers/travel_essentials_controller.rb
@@ -0,0 +1,3 @@
+class TravelEssentialsController < ApplicationController 
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController 
+
+    get '/signup' do
+        erb :signup
+    end
+
+    post '/signup' do
+        
+    end
+end
